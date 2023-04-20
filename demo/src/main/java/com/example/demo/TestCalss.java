7af6490957c5e6e96697362951242347b56b7a0f
Nadia Shahid <113157154+Glxy22@users.noreply.github.com>
2023-04-20 15:07:37 -0500

    Add files via upload
    DEMO

---
+++ demo/src/main/java/com/example/demo/TestCalss.java
@@ -0,0 +1,46 @@
+package com.example.demo;
+
+import java.util.Objects;
+
+public class TestCalss {
+    public static void main(String[] args) {
+
+    }
+}
+
+
+class User {
+
+    private String name;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return name.equals(user.name) && email.equals(user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, email);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
